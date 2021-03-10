5e991254c0661545e204a2653608651dc9ce4c8c
Jackanthon <73250214+Jackanthon@users.noreply.github.com>
2021-03-10 08:09:46 -0600

    Add files via upload

---
+++ GuessWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuesser
+{
+    /// <summary>
+    /// 1. ask for real number 2. if not 50 ask higher or lower
+    /// 3. guess 25 or 75. 
+    /// 4. keep diving by 2 and asking higher or lower
+    /// 5. if number is right at any time stop program
+    /// </summary>
+    class GuessWork
+    {
+        public static int realNum;
+        public int isLower;
+        public int isHigher;
+        public string correctNum;
+    }
+    
+}
+
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int guess;
+            int guesses = 0;
+            int num = random.Next(100);
+            Console.WriteLine("Guess a number 1-100");
+            
+            for (int i = 0; i < 100; i++)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess > 0 || guess < 100)
+                {
+                    if (guess > num)
+                    {
+                        Console.WriteLine("Too high");
+                        Console.WriteLine("Your guess: " + guess);
+                        guesses++;
+                    }
+                    else if (guess < num)
+                    {
+                        Console.WriteLine("Too low");
+                        Console.WriteLine("Your guess: " + guess);
+                        guesses++;
+                    }
+                    else if (guess == num)
+                    {
+                        Console.WriteLine("Correct!!!!!!!");
+                        guesses++;
+                        Console.WriteLine("Guessed taken: " + guesses);
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Number");
+                    return;
+                }
+
+            }
+
+
+        }
+    }
+}
